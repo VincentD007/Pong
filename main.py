febcb19f5c64675dd28c04ee582d85bf124ffe0e
Vincent <vincentedward007@gmail.com>
2024-09-03 19:02:51 -0700

    Added boarders; Added draw_boarders function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
 import random
-
-HEIGHT = 1000
-WIDTH = 1600
+from pong_functions import *
+HEIGHT = 800
+WIDTH = 1200
+clock = pygame.time.Clock()
 
 
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -10,19 +11,32 @@ SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 def main():
+    ball_x, ball_y = 0, 200
+    ball_velocity = 10
     player1_score = 0
     player2_score = 0
+    walls = {
+        "left_goal": pygame.rect.Rect(0, 0, 10, HEIGHT),
+        "right_goal": pygame.rect.Rect(1190, 0, 10, HEIGHT),
+        "top_boarder": pygame.rect.Rect(0, 0, WIDTH, 10),
+        "bottom_boarder": pygame.rect.Rect(0, HEIGHT-10, WIDTH, 10)
+        }
 
+    ball_x, ball_y = 0, 200
     play_game = True
+
     while play_game:
+        clock.tick(60)
+        ball = pygame.rect.Rect(ball_x, ball_y, 20, 20)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 play_game = False
+        SCREEN.fill((0, 0, 0))    
 
+        pygame.draw.rect(SCREEN, (255, 255, 255), ball)
+        ball_x += 10
+        draw_boarders(SCREEN, walls)
 
-
-
-        SCREEN.fill((0, 0, 0))
         pygame.display.update()
     pygame.quit()
 
--- pong_functions.py
+++ pong_functions.py
@@ -1,1 +1,9 @@
 import pygame
+
+def draw_boarders(Screen, Walls):
+    for wall in Walls:
+        pygame.draw.rect(Screen, (255, 0, 0), Walls[wall])
+    mid_spacing = 25
+    for _ in range(16):
+        pygame.draw.rect(Screen, (255, 255, 255), pygame.rect.Rect((Screen.get_width()/2) - 12, mid_spacing, 20, 50))
+        mid_spacing += 100
