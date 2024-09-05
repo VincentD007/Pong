84bb53b78d9dab427f50c6b289a1a95cfd88469e
Vincent <vincentedward007@gmail.com>
2024-09-04 19:19:02 -0700

    Created Ball class; Added paddles that move up and down within the screen; Started to build check_collision function

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import pygame
 import random
 from pong_functions import *
+
 HEIGHT = 800
 WIDTH = 1200
 clock = pygame.time.Clock()
-
-
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 
 def main():
-    ball_x, ball_y = 0, 200
-    ball_velocity = 10
+    ball = Ball("left")
+    left_paddle = pygame.rect.Rect(100, 325, 25, 150)
+    right_paddle = pygame.rect.Rect(1075, 325, 25, 150)
     player1_score = 0
     player2_score = 0
     walls = {
@@ -22,19 +22,32 @@ def main():
         "bottom_boarder": pygame.rect.Rect(0, HEIGHT-10, WIDTH, 10)
         }
 
-    ball_x, ball_y = 0, 200
     play_game = True
 
     while play_game:
         clock.tick(60)
-        ball = pygame.rect.Rect(ball_x, ball_y, 20, 20)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 play_game = False
-        SCREEN.fill((0, 0, 0))    
+        SCREEN.fill((0, 0, 0))
 
-        pygame.draw.rect(SCREEN, (255, 255, 255), ball)
-        ball_x += 10
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_DOWN]:
+            if right_paddle.y + 10 < 650:
+                right_paddle.y += 10
+        if keys_pressed[pygame.K_UP]:
+            if right_paddle.y - 10 > 0:
+                right_paddle.y -= 10
+        if keys_pressed[pygame.K_LCTRL]:
+            if left_paddle.y + 10 < 650:
+                left_paddle.y += 10
+        if keys_pressed[pygame.K_LSHIFT]:
+            if left_paddle.y - 10 > 0:
+                left_paddle.y -= 10
+
+        pygame.draw.rect(SCREEN, (255, 255, 255), left_paddle)
+        pygame.draw.rect(SCREEN, (255, 255, 255), right_paddle)
+        ball.draw(SCREEN)
         draw_boarders(SCREEN, walls)
 
         pygame.display.update()
--- pong_functions.py
+++ pong_functions.py
@@ -5,5 +5,40 @@ def draw_boarders(Screen, Walls):
         pygame.draw.rect(Screen, (255, 0, 0), Walls[wall])
     mid_spacing = 25
     for _ in range(16):
-        pygame.draw.rect(Screen, (255, 255, 255), pygame.rect.Rect((Screen.get_width()/2) - 12, mid_spacing, 20, 50))
+        pygame.draw.rect(Screen, (255, 255, 255), pygame.rect.Rect((Screen.get_width()/2) - 7, mid_spacing, 14, 50))
         mid_spacing += 100
+
+
+def check_collision(ball, walls):
+    for wall in walls:
+        if pygame.Rect.colliderect(ball, wall):
+            if walls[wall] == "left_goal":
+                pass
+            elif walls[wall] == "right_goal":
+                pass
+            elif walls[wall] == "top_boarder":
+                pass  
+            elif walls[wall] == "bottom_boarder":
+                pass
+
+
+
+class Ball:
+    def __init__(self, spawn_direction) -> None:
+        self.direction = spawn_direction
+        self.spawn_velocity = 5
+        self.x, self.y = 590, 390
+        self.rise, self.run = 10, 10
+        self.object = pygame.rect.Rect(self.x, self.y, 20, 20)
+        self.color = (255, 255, 255)
+
+    def move(self):
+        self.y += self.rise
+        self.x += self.run
+        self.object = pygame.rect.Rect(self.x, self.y, 20, 20)
+
+
+    def draw(self, Screen):
+        pygame.draw.rect(Screen, self.color, self.object)
+
+
