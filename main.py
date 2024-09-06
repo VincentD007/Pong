e850a63e3aa58162081adaba056e586a89087f85
Vincent <vincentedward007@gmail.com>
2024-09-05 20:40:29 -0700

    Ball now reflects off of the Top and Bottom boarders; Two functions added to increase the player score if the player scores

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
-import random
 from pong_functions import *
 
+pygame.init()
 HEIGHT = 800
 WIDTH = 1200
 clock = pygame.time.Clock()
@@ -10,19 +10,18 @@ SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 def main():
-    ball = Ball("left")
-    left_paddle = pygame.rect.Rect(100, 325, 25, 150)
-    right_paddle = pygame.rect.Rect(1075, 325, 25, 150)
-    player1_score = 0
-    player2_score = 0
+    ball = Ball()
+    left_paddle = pygame.rect.Rect(100, 325, 20, 150)
+    right_paddle = pygame.rect.Rect(1075, 325, 20, 150)
+    left_player_score = 0
+    right_player_score = 0
+    play_game = True
     walls = {
         "left_goal": pygame.rect.Rect(0, 0, 10, HEIGHT),
         "right_goal": pygame.rect.Rect(1190, 0, 10, HEIGHT),
         "top_boarder": pygame.rect.Rect(0, 0, WIDTH, 10),
         "bottom_boarder": pygame.rect.Rect(0, HEIGHT-10, WIDTH, 10)
         }
-
-    play_game = True
 
     while play_game:
         clock.tick(60)
@@ -44,10 +43,18 @@ def main():
         if keys_pressed[pygame.K_LSHIFT]:
             if left_paddle.y - 10 > 0:
                 left_paddle.y -= 10
-
         pygame.draw.rect(SCREEN, (255, 255, 255), left_paddle)
         pygame.draw.rect(SCREEN, (255, 255, 255), right_paddle)
+        ball.move()
+        handle_collisions(ball, walls, left_paddle, right_paddle)
+        if left_scored(walls, ball):
+            left_player_score += 1
+            ball.reset()
+        elif right_scored(walls, ball):
+            right_player_score += 1
+            ball.reset()
         ball.draw(SCREEN)
+        print(left_player_score)
         draw_boarders(SCREEN, walls)
 
         pygame.display.update()
--- pong_functions.py
+++ pong_functions.py
@@ -1,44 +1,74 @@
 import pygame
+import random
+import math
+pygame.init()
 
 def draw_boarders(Screen, Walls):
     for wall in Walls:
-        pygame.draw.rect(Screen, (255, 0, 0), Walls[wall])
+        pygame.draw.rect(Screen, (255, 255, 255), Walls[wall])
     mid_spacing = 25
     for _ in range(16):
         pygame.draw.rect(Screen, (255, 255, 255), pygame.rect.Rect((Screen.get_width()/2) - 7, mid_spacing, 14, 50))
         mid_spacing += 100
 
 
-def check_collision(ball, walls):
-    for wall in walls:
-        if pygame.Rect.colliderect(ball, wall):
-            if walls[wall] == "left_goal":
-                pass
-            elif walls[wall] == "right_goal":
-                pass
-            elif walls[wall] == "top_boarder":
-                pass  
-            elif walls[wall] == "bottom_boarder":
-                pass
+def left_scored(walls, ball):
+    return pygame.Rect.colliderect(ball.rect, walls["right_goal"])
 
+
+def right_scored(walls, ball):
+    return pygame.Rect.colliderect(ball.rect, walls["left_goal"])
+
+
+def handle_collisions(ball, walls, left_paddle, right_paddle):
+    def check_first_collision(ball_object):
+        if not ball.firsthit:
+            ball_object.velocity += 6
+            ball_object.firsthit = True
+
+    if pygame.Rect.colliderect(ball.rect, walls["top_boarder"]):
+        angle = ball.direction % math.pi
+        if angle < math.pi/2:
+            ball.direction -= ball.direction*2
+        elif angle > math.pi/2:
+            ball.direction += (math.pi - ball.direction) * 2
+    elif pygame.Rect.colliderect(ball.rect, walls["bottom_boarder"]):
+        angle = ball.direction % (2 * math.pi)
+        if angle < 3 * math.pi/2:
+            ball.direction -= (math.pi/2 - ((3 * math.pi/2) - angle)) * 2
+        elif angle > 3 * math.pi/2:
+            ball.direction += (2 * math.pi - angle) * 2
+    if pygame.Rect.colliderect(ball.rect, left_paddle):
+        ball.direction -= math.pi
+        check_first_collision(ball)
+
+    elif pygame.Rect.colliderect(ball.rect, right_paddle):
+        ball.direction += math.pi
+        check_first_collision(ball)
 
 
 class Ball:
-    def __init__(self, spawn_direction) -> None:
-        self.direction = spawn_direction
-        self.spawn_velocity = 5
-        self.x, self.y = 590, 390
-        self.rise, self.run = 10, 10
-        self.object = pygame.rect.Rect(self.x, self.y, 20, 20)
+    def __init__(self) -> None:
+        self.firsthit = False
+        self.direction = random.choice([0, math.pi])
+        self.velocity = 4
+        self.rect = pygame.rect.Rect(590, 390, 20, 20)
         self.color = (255, 255, 255)
 
+
     def move(self):
-        self.y += self.rise
-        self.x += self.run
-        self.object = pygame.rect.Rect(self.x, self.y, 20, 20)
+        distance_x = (math.cos(self.direction) * self.velocity)
+        self.rect.x += distance_x
+        distance_y = (math.sin(self.direction) * self.velocity)
+        self.rect.y -= distance_y
 
 
     def draw(self, Screen):
-        pygame.draw.rect(Screen, self.color, self.object)
+        pygame.draw.rect(Screen, self.color, self.rect)
 
 
+    def reset(self):
+        self.firsthit = False
+        self.direction = random.choice([0, math.pi])
+        self.velocity = 5
+        self.rect = pygame.rect.Rect(590, 390, 20, 20)
